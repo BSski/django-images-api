a9276a6d40323072ce2546e77c9f729291a38e75
bsski <50927653+BSski@users.noreply.github.com>
2022-06-24 00:42:37 +0200

    apps tweak

--- website/users/apps.py
+++ website/users/apps.py
@@ -5,8 +5,8 @@ class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
-    def ready(self):
-        from .models import UserTier
-
-        for tier in UserTier.objects.all():
-            tier.save()
+    # def ready(self):
+    #     from .models import UserTier
+    #
+    #     for tier in UserTier.objects.all():
+    #         tier.save()
