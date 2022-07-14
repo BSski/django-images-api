94a1c26d4d0c41562a692559983ada9d6a18b764
bsski <50927653+BSski@users.noreply.github.com>
2022-07-14 02:26:22 +0200

    test tweaks

--- website/images/tests/test_models.py
+++ website/images/tests/test_models.py
@@ -16,26 +16,35 @@ class ImageModelTest(TestCase):
         #     name="TestImage",
         #     image=""
         # )
-        cls.test_image = SimpleUploadedFile("testImage.jpg", b"file_content", content_type="image/jpeg")
+        cls.test_image = SimpleUploadedFile(
+            "testImage.jpg", b"file_content", content_type="image/jpeg"
+        )
         cls.s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_S3_REGION_NAME,
+            "s3",
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_S3_REGION_NAME,
+            endpoint_url=settings.LOCALSTACK_ENDPOINT_URL,
         )
 
     def test_initial_s3_bucket_is_empty(self):
-        s3_objects = self.s3_client.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
-        self.assertEqual("Contents" in s3_objects, True)
+        s3_objects = ImageModelTest.s3_client.list_objects_v2(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME
+        )
+        self.assertEqual("Contents" in s3_objects, False)
 
     def test_image_can_be_uploaded_to_s3_bucket(self):
-        self.client.post(reverse('images:api-root'), {'image': self.test_image})
-        s3_objects = self.s3_client.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+        # This way of posting doesn't work.
+        self.client.post(
+            reverse("images:api-root"), {"image": ImageModelTest.test_image}
+        )
+
+        s3_objects = ImageModelTest.s3_client.list_objects_v2(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME
+        )
         self.assertEqual("Contents" in s3_objects, True)
 
 
-
-#
 # ### example code below
 #
 # from datetime import date
